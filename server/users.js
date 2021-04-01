acc0a7696245ee390321e46400b1ceac85885940
Alena <alena.nik.digital@gmail.com>
2021-04-01 15:40:16 +0200

    add user

--- server/users.js
+++ server/users.js
@@ -1,13 +1,12 @@
 const users = [];
 
-const addUser = ({ id, name, room, error }) => {
+const addUser = ({ id, name, room }) => {
   transformText(name);
   transformText(room);
 
   const existingUser = users.find((user) => user.room === room && user.name === name);
-  if (existingUser) {
-    return { error: 'Username is taken' }
-  }
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
 
   const user = { id, name, room };
   users.push(user);
@@ -22,9 +21,8 @@ const removeUser = ({ id }) => {
   }
 }
 
-const getUser = ({ id }) => {
-  return users.find((user) => user.id === id)
-}
+const getUser = (id) => users.find((user)=> user.id === id);
+
 
 const getUsersInRoom = ({ room }) => {
   return users.filter((user) => user.room === room);
