641a6fa4acdf77564cabe869b8862b0fa425a724
Alena <alena.nik.digital@gmail.com>
2021-04-01 10:45:09 +0200

    allow connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,34 @@
 const express = require('express');
 const socketio = require('socket.io');
 
+const router = require('./router');
+
 const http = require('http');
 const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+
+const io = socketio(server, {
+  cors: {
+    origin: "your origin",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+// socket connects with the client
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }) => {
+    console.log(name, room)
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+app.use(router);
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
